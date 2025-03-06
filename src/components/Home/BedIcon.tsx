5884df9c417e7fa444105a4c3f6ad1b3f10b3ac2
Phattharaphon Jansanga <65160078@go.buu.ac.th>
2025-03-06 18:56:03 +0700

    fix bedicon

--- src/components/Home/BedIcon.tsx
+++ src/components/Home/BedIcon.tsx
@@ -6,32 +6,39 @@ import Sit from "../../assets/Sit.png";
 import NotHere from "../../assets/NotHere.png";
 import Default from "../../assets/default.png";
 
-interface BedIconProps {
-  bedsensors: any; // แก้ไข type ให้ถูกต้องตามข้อมูล bedsensor ของคุณ
+interface BedSensor {
+  sensor_type: string;
+  history_value_sensor: {
+    history_value_sensor_value: string;
+  }[];
 }
 
-const bedIcon: React.FC<BedIconProps> = ({ bedsensors }) => {
-  const iconMap: { [key: string]: any } = {
-    "นอนตะแคงซ้าย": LeftSide,
-    "นอนตะแคงขวา": RightSide,
-    "นอนตรง": Straight,
-    "นั่ง": Sit,
-    "ไม่อยู่ที่เตียง": NotHere,
+interface BedIconProps {
+  bedsensors: BedSensor[];
+}
+
+const BedIcon: React.FC<BedIconProps> = ({ bedsensors }) => {
+  const iconMap: { [key: string]: string } = {
+    นอนตะแคงซ้าย: LeftSide,
+    นอนตะแคงขวา: RightSide,
+    นอนตรง: Straight,
+    นั่ง: Sit,
+    ไม่อยู่ที่เตียง: NotHere,
   };
 
-  return bedsensors?.map((bedsensor: any, index: number) => {
+  return bedsensors?.map((bedsensor, index) => {
     const sensorValue =
-      bedsensor.history_value_sensor.slice(-1)[0]
-        ?.history_value_sensor_value ?? "";
+      bedsensor.history_value_sensor.slice(-1)[0]?.history_value_sensor_value ??
+      "";
 
     const iconPath = iconMap[sensorValue] || Default;
 
     return (
       <div key={index} className="flex items-center">
-        <img src={iconPath} alt={sensorValue} className="w-40 h-40"/>
+        <img src={iconPath} alt={sensorValue} className="w-40 h-40" />
       </div>
     );
   });
 };
 
-export default bedIcon;
+export default BedIcon;
