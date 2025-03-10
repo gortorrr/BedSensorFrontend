ae9c22b32a9d39c148a3934f703ca84e5c462ad6
Supphakorn Saengjit <65160196@go.buu.ac.th>
2025-03-10 17:16:23 +0700

    service loadAllSensorFree

--- src/services/sensorService.ts
+++ src/services/sensorService.ts
@@ -6,4 +6,14 @@ export const sensorService = {
     const response = await http.get(`sensors/getValueSensor/${sensor_id}`);
     return response.data; // นำข้อมูลที่ได้จาก response มาใช้
   },
+
+  async loadAllSensorFree(): Promise<Sensor[]> {
+    try {
+      const response = await http.get("/sensors/sensorFree/all");
+      return response.data; // นำข้อมูลที่ได้จาก response มาใช้
+    } catch (error) {
+      console.error("Error loading all sensors:", error);
+      throw error; // หากเกิดข้อผิดพลาดให้โยนข้อผิดพลาดออกไป
+    }
+  },
 };
