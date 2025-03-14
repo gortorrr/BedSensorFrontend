37827e3bb447a25fa7e45c760e13cc1eb114bdc9
Sirapob Wongtim <65160085@go.buu.ac.th>
2025-03-14 16:11:30 +0700

    HistorySensorStore

---
+++ src/store/historyValueSensorStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { History_Value_Sensor } from "../types/history_value_sensor";
+import { historyValueSensorService } from "../services/historyValueSensorService";
+
+
+interface historyValueSensorStore {
+  HistorySensor: History_Value_Sensor[];
+  load1DayHistoryValue(sensor_id: number,date_str: string): Promise<void>
+}
+export const useSensorNotificationsConfigStore =
+  create<historyValueSensorStore>((set) => ({
+    HistorySensor: [],
+    load1DayHistoryValue: async (sensor_id: number,date_str: string) => {
+      set({ HistorySensor: [] });
+      const res =
+        await historyValueSensorService.load1DayHistoryValue(sensor_id,date_str);//2025-03-05
+      console.log("res data ตรงนี้ ที่ store", res); // ✅ ป้องกัน error
+      if (res) {
+        set({ HistorySensor: Array.isArray(res) ? res : [res] }); // ✅ ป้องกัน error
+      }
+    },
+  }));
--- src/types/history_value_sensor.ts
+++ src/types/history_value_sensor.ts
@@ -1,4 +1,5 @@
 export interface History_Value_Sensor {
+  sensor_id?:number;
   history_value_sensor_id?: number;
   history_value_sensor_value: string;
   history_value_sensor_time?: string;
