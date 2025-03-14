f00cde9347c242318347188d8e9d3621d573c9fd
Sirapob Wongtim <65160085@go.buu.ac.th>
2025-03-14 17:18:21 +0700

    success test loadNotiByPtAndSensor

--- src/store/notificationStore.ts
+++ src/store/notificationStore.ts
@@ -3,23 +3,23 @@ import { notificationService } from "../services/notificationService";
 import { Notification } from "../types/notification";
 
 interface NotificationStore {
+  NotificationByPatientAndSensor:Notification[]
   showAlert: boolean;
   setShowAlert: (value: boolean) => void;
-  loadAllNotificationByPatient: (patient_id: number, sensor_id: number) => Promise<Notification | null>;
+  loadAllNotificationByPatient: (patient_id: number, sensor_id: number) => Promise<void | null>;
 }
 
 export const useNotificationStore = create<NotificationStore>((set) => ({
+  NotificationByPatientAndSensor:[],
   showAlert: false,
   setShowAlert: (value) => set({ showAlert: value }),
 
   loadAllNotificationByPatient: async (patient_id: number, sensor_id: number) => {
-    try {
+    set({NotificationByPatientAndSensor:[]})
       const res = await notificationService.loadAllNotificationByPatient(patient_id, sensor_id);
       console.log("✅ API Response NotificationByPatient :", res);
-      return (res as Notification) ?? null; // ✅ ถ้า res เป็น undefined → เปลี่ยนเป็น null
-    } catch (error) {
-      console.error("❌ โหลดข้อมูลแจ้งเตือนล้มเหลว:", error);
-      return null; // ✅ คืนค่า null ถ้าเกิดข้อผิดพลาด
-    }
-  },
+      if (res) {
+        set({ NotificationByPatientAndSensor: Array.isArray(res) ? res : [res] }); // ✅ ป้องกัน error
+      } // ✅ ถ้า res เป็น undefined → เปลี่ยนเป็น null
+  }
 }));
