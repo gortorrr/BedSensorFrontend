97944436e97b099a49685b1ac419bbf601106b6e
33 Sirikorn <maisirikorn.500@gmail.com>
2025-05-14 02:01:27 +0700

    decorate login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,31 +7,48 @@ const Login: React.FC = () => {
   };
   return (
     <div
-      className="min-h-screen w-full bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('/your-background.jpg')" }}
+      className="min-h-screen w-full bg-cover bg-center flex items-center justify-center "
+      style={{ backgroundImage: "url('src/assets/BGlogin.png')" }}
     >
-      <div className="bg-emerald-100 shadow-xl rounded-2xl w-100 h-100 p-4 flex flex-col justify-start items-center pt-12">
+      <div className="relative z-10 shadow-xl rounded-2xl w-[550px] h-[640px] p-8 flex flex-col justify-start items-center pt-12"
+      style={{
+          background: 'linear-gradient(135deg, rgba(201, 201, 201, 0.5) 0%, rgba(196, 196, 196, 0.1) 100%)',
+          backdropFilter: 'blur(8px)'
+        }}
+      >
         {/* โลโก้ */}
         <div className="mb-4">
-          <img src="/logo.png" alt="Logo" className="h-12 mx-auto" />
+          <img src="/src/assets/BuuLogo2.png" alt="Logo" className="h-35 mx-auto" />
         </div>
 
         {/* ชื่อผู้ใช้ */}
-        <label className="self-start text-sm text-gray-700 mb-1">
-          ชื่อผู้ใช้
+        <label className="self-start text-[#FFFFFF] text-2xl font-light mb-6">
+          ชื่อผู้ใช้งาน
         </label>
         <input
           type="text"
-          className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="กรุณากรอกชื่อผู้ใช้งาน"
+          className="w-full mb-6 h-14 px-3 py-2 pr-10 rounded-md focus:outline-none focus:ring-3 focus:ring-[#B7D6DE]"
+          style={{
+              background: 'rgba(242, 242, 242, 0.6)',
+              backdropFilter: 'blur(4px)',
+              border: '1px solid rgba(200, 200, 200, 0.8)'
+            }}
         />
 
         {/* รหัสผ่าน */}
-        <label className="self-start text-sm text-gray-700 mb-1">
+        <label className="self-start text-[#FFFFFF] text-2xl font-light mb-6">
           รหัสผ่าน
         </label>
         <input
           type="password"
-          className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="กรุณากรอกรหัสผ่าน"
+          className="w-full mb-8 h-14 px-3 py-2 pr-10  rounded-md focus:outline-none focus:ring-3 focus:ring-[#B7D6DE]"
+          style={{
+              background: 'rgba(242, 242, 242, 0.6)',
+              backdropFilter: 'blur(4px)',
+              border: '1px solid rgba(200, 200, 200, 0.8)'
+            }}
         />
         {/* <button
             type="button"
@@ -43,7 +60,7 @@ const Login: React.FC = () => {
         {/* ปุ่มเข้าสู่ระบบ */}
         <br />
         <button
-          className="w-4/5 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+          className="w-4/5 py-2 h-14 bg-[#A5D9D0] text-gray-800 font-medium text-xl transition-colors rounded-md hover:bg-[#9DDBC1] duration-300 hover:scale-105 active:scale-95 cursor-pointer"
           onClick={login}
         >
           เข้าสู่ระบบ
