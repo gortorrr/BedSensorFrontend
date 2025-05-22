bfee2ec25c9ebf74f22e0eda873d5b4d5f98951e
Peerada Wangyaichim <65160184@go.buu.ac.th>
2025-05-23 00:21:50 +0700

    Page Noti History

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -45,7 +45,7 @@ const menus: MenuItem[] = [
       { name: "ผู้ป่วย", link: "/patient-management", icon: FaUserInjured },
       { name: "ผู้ใช้งานระบบ", link: "/user-management", icon: FaUserCog },
       { name: "จัดการสถานที่", link: "/bed-management", icon: RiHospitalFill },
-      { name: "ประวัติการแจ้งเตือน", link: "/", icon: VscBellDot },
+      { name: "ประวัติการแจ้งเตือน", link: "/noti-history", icon: VscBellDot },
     ],
   },
 ];
